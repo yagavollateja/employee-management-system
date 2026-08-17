cc6839630c735911479a85502082353ac078e6ff
YAGAVOLLA TEJA <yagavollateja@gmail.com>
2026-08-17 23:19:16 +0530

    Update API base URL to use VITE environment variable

--- ems-frontend/src/services/EmployeeServices.js
+++ ems-frontend/src/services/EmployeeServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api/employees';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const ListOfEmployees =()=>  axios.get(API_BASE_URL);
 export const createEmployee = (employee) => axios.post(API_BASE_URL,employee);
 export const getEmployee = (employeeId)=>axios.get(API_BASE_URL+'/'+employeeId);
